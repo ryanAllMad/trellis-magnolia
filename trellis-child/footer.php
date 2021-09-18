0c39c9807a42296274ed74a33aa369c7841e17b8
Ryan <46852695+ryanAllMad@users.noreply.github.com>
2021-09-17 17:08:15 -0700

    full width Footer widget areas
    Overwrote widget areas with new widget areas, minor styling change

--- trellis-child/footer.php
+++ trellis-child/footer.php
@@ -1,8 +1,26 @@
 <?php mvt_footer_before(); ?>
 		<footer class="footer">
 			<div class="wrapper wrapper-footer">
+				<div class="footer-container">
+				<div class="top-footer-widget-area">
+				<?php mv_trellis_output_sidebar( 'top-footer' ); ?>
+				<!--
+					place widget-areas in Options Multiple of class init.php file to control widgget areas
+			
+			/**
+			 * Set up standard variables on the site.
+			 *
+			 * These can be overridden with a Child theme by setting the
+			 * options in an action using the `mv_trellis_parent_loaded` hook
+			 */
+			Options::set_multiple(
+				-->
+				</div><!--top footer widget area-->
+				<div class="bottom-footer-widget-area">
+				<?php mv_trellis_output_sidebar( 'mid-footer' ); ?>
+				</div><!--bottom footer widget area-->
+				</div><!--footer container-->
 				<?php
-				/*mvt_footer_top();*/
 				mvt_footer_bottom();
 				?>
 			</div>
--- trellis-child/functions.php
+++ trellis-child/functions.php
@@ -7,6 +7,20 @@ function mv_trellis_child_set_options() {
             'child_version' => $child_version,
             'child_css'     => get_stylesheet_directory_uri() . "/assets/dist/css/main.$child_version.css",
             'child_js'      => get_stylesheet_directory_uri() . "/assets/dist/js/main.$child_version.js",
+            'widget_areas'        => [
+                'sidebar'       => [
+                    'name' => __( 'Primary Sidebar', 'mediavine' ),
+                    'desc' => __( 'The primary sidebar', 'mediavine' ),
+                ],
+                'top-footer'   => [
+                    'name' => __( 'Top Footer Area', 'mediavine' ),
+                    'desc' => __( 'The top footer widget area', 'mediavine' ),
+                ],
+                'mid-footer'  => [
+                    'name' => __( 'Mid Footer Area', 'mediavine' ),
+                    'desc' => __( 'The bottom footer widget area', 'mediavine' ),
+                ],
+            ],
         ]
     );
   }
